332df634fdf377cbe65fb1eef5c802655195e35c
Daniel Seifert <git@daniel-seifert.com>
2025-12-04 09:48:23 +0100

    improve Http API payload for ELK stacks

--- Handlers/HttpApiHandler.php
+++ Handlers/HttpApiHandler.php
@@ -17,16 +17,31 @@ declare(strict_types=1);
 
 namespace D3\OxLogIQ\Handlers;
 
+use D3\OxLogIQ\MonologConfiguration;
+use D3\OxLogIQ\Release\ReleaseServiceInterface;
+use DateTime;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
+use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\Eshop\Core\ShopVersion;
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
+use OxidEsales\EshopCommunity\Internal\Framework\Logger\Configuration\MonologConfigurationInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class HttpApiHandler extends AbstractProcessingHandler
 {
     protected Client $httpClient;
 
-    public function __construct(protected $endpoint, $apiKey, $level = Logger::DEBUG, $bubble = true, Client $client = null)
-    {
+    public function __construct(
+        protected $endpoint,
+        $apiKey,
+        $level = Logger::DEBUG,
+        $bubble = true,
+        Client $client = null
+    ){
         parent::__construct($level, $bubble);
 
         $this->httpClient = $client ?? new Client([
@@ -39,23 +54,42 @@ class HttpApiHandler extends AbstractProcessingHandler
 
     protected function write(array $record): void
     {
-        $this->httpClient->post(
-            $this->endpoint,
-            [
-                'json' => [
-                    'message' => $record['message'],
-                    'context' => $record['context'],
-                    'level' => $record['level_name'],
-                    '@timestamp' => $record['datetime']->format(DATE_ATOM),
-                    'channel' => $record['channel'],
-                    'release' => '',        // retrieve it from configuration
-                    'sid'   => $record['extra']['sid'],
-                    'uid'   => $record['extra']['uid'],
-                    'class'   => $record['extra']['class'] ?? '',
-                    'function'   => $record['extra']['function'] ?? '',
-                    'line'   => $record['extra']['line'] ?? '',
+        try {
+            $this->httpClient->post(
+                $this->endpoint,
+                [
+                    'json' => [
+                        'message'       => $record['message'],
+                        'context'       => $record['context'],
+                        'log.level'     => $record['level_name'],
+                        'log.logger'    => $record['channel'],
+                        '@timestamp'    => $record['datetime'] instanceof DateTime ?
+                            $record['datetime']->format('c') :
+                            date( 'c' ),
+                        'event.dataset' => 'OXID eShop ' . ShopVersion::getVersion(),
+                        'host.name'     => Registry::getConfig()->getShopUrl(),
+                        'release'       => $this->getRelease(),
+                        'call'          => [
+                            'sid'      => $record['extra']['sid'],
+                            'uid'      => $record['extra']['uid'],
+                            'class'    => $record['extra']['class'] ?? '',
+                            'function' => $record['extra']['function'] ?? '',
+                            'line'     => $record['extra']['line'] ?? ''
+                        ]
+                    ]
                 ]
-            ]
-        );
+            );
+        } catch ( GuzzleException) {}
+    }
+
+    protected function getRelease(): string
+    {
+        /** @var ReleaseServiceInterface $service */
+        try {
+            $service = ContainerFactory::getInstance()->getContainer()->get(ReleaseServiceInterface::class);
+            return $service->getRelease();
+        } catch ( NotFoundExceptionInterface|ContainerExceptionInterface) {
+            return '';
+        }
     }
 }
